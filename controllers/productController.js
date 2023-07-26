be29d94381840cb001ba98ac8997001bd026ab0e
Subham <Subham@gmail.com>
2023-07-26 17:15:21 +0530

    Search Functionality

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,14 +4,23 @@ const Product = require('../models/Product');
 // Get all products
 const getAllProducts = async (req, res) => {
     try {
-        const products = await Product.find({});
-        res.status(200).json(products);
+        const { searchQuery } = req.query;
+
+        // If searchQuery is provided, perform a search
+        if (searchQuery) {
+            const regex = new RegExp(searchQuery, 'i');
+            const products = await Product.find({ name: regex });
+            res.status(200).json(products);
+        } else {
+            // Otherwise, fetch all products
+            const products = await Product.find({});
+            res.status(200).json(products);
+        }
     } catch (err) {
         console.error('Error fetching products:', err);
         res.status(500).json({ message: 'Failed to fetch products' });
     }
 };
-
 // Create a new product
 const createProduct = async (req, res) => {
     try {
--- server.js
+++ server.js
@@ -41,6 +41,7 @@ app.use('/api/orders', orderRoutes);
 
 // Checkout routes
 app.post('/api/checkout/order', checkoutController.createOrder);
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });

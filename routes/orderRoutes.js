7c7cf8c386223678674e8554ab963dc02e67c97c
Subham <Subham@gmail.com>
2023-07-27 16:50:06 +0530

    order api changes

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -15,6 +15,18 @@ router.get('/:userId', async (req, res) => {
     }
 });
 
+// Get all orders
+router.get('/all', async (req, res) => {
+    try {
+        const orders = await Order.find({}).sort({ date: -1 });;
+        res.status(200).json(orders);
+    } catch (error) {
+        console.error('Error fetching orders:', error);
+        res.status(500).json({ message: 'Failed to fetch orders' });
+    }
+});
+
+
 // Create a new order
 router.post('/', async (req, res) => {
     try {
@@ -41,6 +53,22 @@ router.put('/:orderId', async (req, res) => {
     } catch (error) {
         console.error('Error updating order status:', error);
         res.status(500).json({ error: 'Failed to update order status.' });
+    }
+});
+
+
+// Delete an order by ID
+router.delete('/:orderId', async (req, res) => {
+    try {
+        const orderId = req.params.orderId;
+        const deletedOrder = await Order.findByIdAndDelete(orderId);
+        if (!deletedOrder) {
+            return res.status(404).json({ message: 'Order not found' });
+        }
+        res.status(200).json({ message: 'Order deleted successfully', order: deletedOrder });
+    } catch (error) {
+        console.error('Error deleting order:', error);
+        res.status(500).json({ message: 'Failed to delete order' });
     }
 });
 
